3573eb9abb5317ea8e2b123c7a7952c276a74c36
Omote <immanuelomote@gmail.com>
2022-10-03 10:36:48 +0000

    using library function strcpy

--- mallocstring.c
+++ mallocstring.c
@@ -10,10 +10,7 @@ int main(void)
 
     char *t = malloc(strlen(s) + 1);
 
-    for(int i = 0, n = strlen(s) + 1; i < n; i++)
-    {
-        t[i] = s[i];
-    }
+    strcpy(t, s);
 
     t[0] = toupper(t[0]);
 
