1a84951323e4670d2623f1c7fd450c5bec2231f7
Omote <immanuelomote@gmail.com>
2022-10-03 10:33:14 +0000

    Copy a string

---
+++ mallocstring.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<string.h>
+#include<ctype.h>
+#include<stdlib.h>
+#include<cs50.h>
+
+int main(void)
+{
+    char *s = get_string("s: ");
+
+    char *t = malloc(strlen(s) + 1);
+
+    for(int i = 0, n = strlen(s) + 1; i < n; i++)
+    {
+        t[i] = s[i];
+    }
+
+    t[0] = toupper(t[0]);
+
+    printf("s: %s\n", s);
+    printf("t: %s\n", t);
+}
